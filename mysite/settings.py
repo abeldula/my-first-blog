7f7b9b7c89d088b492a3199f0aba98ac57ec7984
abeldula <abel.server@gmail.com>
2023-09-29 16:41:44 +0800

    Security enhanced and created a user login and logout

--- mysite/settings.py
+++ mysite/settings.py
@@ -127,3 +127,5 @@ ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = '/'
